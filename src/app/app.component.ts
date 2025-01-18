278bcf3b614a52c75bdc5d9884625ae5f75af56a
Kotturi858 <kotturi858bharath@gmail.com>
2025-01-19 02:28:34 +0530

    added navbar
    added material lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TooltipPosition, MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTooltipModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio';
+  navButtons = [
+    { label: 'HOME', icon: 'home' },
+    { label: 'THEME', icon: 'person' },
+    { label: 'PROJECTS', icon: 'tactic' },
+    { label: 'CONTACT ME', icon: 'contact_mail' },
+    { label: 'RESUME', icon: 'description' },
+    { label: 'ROUTINE', icon: 'routine' },
+  ];
 }
